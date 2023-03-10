f263957ebfca7aa3efb51c730f37f0dfb6218a9b
hzp <hzhouping@outlook.com>
2023-03-10 17:38:35 +0800

    初始化配置

---
+++ js/components/index.js
@@ -0,0 +1,5 @@
+import { TimeFormatted } from './time-formatted.js'
+import { LiveTimer } from './live-timer.js'
+
+customElements.define('time-formatted', TimeFormatted)
+customElements.define('live-timer', LiveTimer)
