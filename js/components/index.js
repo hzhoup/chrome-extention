ae4b27e81273ba673ef71ea4abe059149a6f3546
hzp <hzhouping@outlook.com>
2023-03-11 09:19:19 +0800

    chore: 重构页面布局

--- js/components/index.js
+++
@@ -1,5 +0,0 @@
-import { TimeFormatted } from './time-formatted.js'
-import { LiveTimer } from './live-timer.js'
-
-customElements.define('time-formatted', TimeFormatted)
-customElements.define('live-timer', LiveTimer)
